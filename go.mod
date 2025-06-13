a2e64e833edfe863bab43b71c706ecf73eed6194
Taras <505555+ribtoks@users.noreply.github.com>
2025-06-13 15:57:45 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PrivateCaptcha/status
+
+go 1.23.4
+
+require github.com/cstate/cstate v6.0.0+incompatible // indirect
---
+++ publish_to_pages.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+rm .hugo_build.lock
+
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+DEPLOY_BRANCH="${DEPLOY_BRANCH:-pages}"
+
+echo "Checking out ${DEPLOY_BRANCH} branch into public"
+git worktree add -B ${DEPLOY_BRANCH} public origin/${DEPLOY_BRANCH}
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo --ignoreCache
+
+#mkdir -p public/.github/workflows
+#cp -v .github/workflows/deploy.yaml public/.github/workflows/
+
+echo "Updating ${DEPLOY_BRANCH} branch"
+cd public && git add --all && git commit -m "Publishing to pages (publish.sh)"
+
+#echo "Pushing to github"
+#git push --all
